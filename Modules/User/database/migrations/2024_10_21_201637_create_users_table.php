f541574ef5fc85765a6fa9ffa953b73a3dfac404
Abood-91 <abdqlfa2000@gmail.com>
2024-10-26 19:37:03 +0300

    Doctor role added

--- Modules/User/database/migrations/2024_10_21_201637_create_users_table.php
+++ Modules/User/database/migrations/2024_10_21_201637_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // This will create an unsigned big integer
+            $table->id();
             $table->string('name');
-            $table->string('email')->unique(); // Add parentheses to 'unique'
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'receptionist', 'doctor']);
